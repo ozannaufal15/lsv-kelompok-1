a31d3e5e8726acfb08b0f89bb3ad954c7be705c7
ozannaufal15 <ozan.naufal15@gmail.com>
2023-11-30 08:28:33 +0800

    Test

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ChirpController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,4 +19,16 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('users', 'App\Http\Controllers\UserController');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::resource('chirps', ChirpController::class)->only(['index', 'store', 'edit', 'update', 'destroy'])->middleware(['auth']);
+
+require __DIR__ . '/auth.php';
